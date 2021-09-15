5338f16d5c03ed3e6094a17d888c65129a332750
ahmed.BK <ahmedbk5080@gmail.com>
2021-09-15 18:43:12 +0200

    redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {useDispatch, useSelector} from 'react-redux';
+import TodoList from './Components/Tasklist';
+import AddTask from './Components/AddTask';
+import { filterTodo } from './Redux/Actions/action';
+import { Button } from 'react-bootstrap';
+
 
 function App() {
+  const todos = useSelector(state=>state.todos);
+  const filter = useSelector(state=>state.filter);
+  const dispatch = useDispatch()
+  
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button style={{width:'100px',marginLeft:'10px', marginRight:'10px'}} onClick={()=>dispatch(filterTodo('All'))} className="btn btn-secondary">All</Button>
+      <Button style={{width:'100px',marginLeft:'10px', marginRight:'10px'}} onClick={()=>dispatch(filterTodo(true))}  className="btn btn-secondary">Done</Button>
+      <Button style={{width:'100px',marginLeft:'10px', marginRight:'10px'}} onClick={()=>dispatch(filterTodo(false))}  className="btn btn-secondary">UnDone</Button><br/><br/>
+      <TodoList todos={filter==='All'? todos : todos.filter((elm)=> elm.isDone===filter)}/>
+      <hr></hr>
+      <br></br>
+      <span style={{fontWeight:'bold', fontFamily:'Comic Sans MS',}}> ADD NEW TASK </span>
+      <br></br>
+      <AddTask/>
     </div>
   );
+
 }
 
 export default App;
